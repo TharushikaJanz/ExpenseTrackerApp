fbce61f232675b6f3b1edca517c810f2406bdac3
tharushi <tharushika@sutratechnologies.com>
2023-05-13 12:25:09 +0530

    Header color changed

--- components/Screens/AppPage.js
+++ components/Screens/AppPage.js
@@ -1,12 +1,37 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 import { MyStack } from "../MyStack";
 
 const AppPage = ({ navigation }) => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleShowPicker = () => {
+    setShowPicker(true);
+  };
+
+  const handleDateChange = (event, date) => {
+    setShowPicker(false);
+    if (date) {
+      setSelectedDate(date);
+    }
+  };
+
   return (
     <MyStack>
-      <Text>Hiii</Text>
+      <TouchableOpacity onPress={handleShowPicker}>
+        <Text>Select Date</Text>
+      </TouchableOpacity>
+      {showPicker && (
+        <DateTimePicker
+          value={selectedDate}
+          mode="date"
+          display="default"
+          onChange={handleDateChange}
+        />
+      )}
     </MyStack>
   );
 };
